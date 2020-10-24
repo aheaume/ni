247094e31c9d8ce71bb3aea6d64b3d16edd2deeb
Alexandre Héaumé <aheaume@users.noreply.github.com>
2020-10-24 18:46:44 +0200

    Update configuration-template.nix

--- configuration-template.nix
+++ configuration-template.nix
@@ -25,7 +25,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config

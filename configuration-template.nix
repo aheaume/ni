7a8593e882f4818686d5a85aab1a4e50dd5aff41
Alexandre Héaumé <aheaume@users.noreply.github.com>
2020-10-24 18:10:27 +0200

    Update configuration-template.nix

--- configuration-template.nix
+++ configuration-template.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running 'nixos-help').
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -10,113 +10,90 @@
       ./hardware-configuration.nix
     ];
 
-  # Specify the encrypted disk
-  boot.initrd.luks.devices.root = {
-    device = "##device##2"; 
-    preLVM = true;
-  };
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Networking setup
-  networking.hostName = "##username##";
-  
-  # Enable NetworkManager
-  networking.networkmanager.enable = true;
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Root password
-  users.users.root.initialHashedPassword = "##rootpasswd##";
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
 
-  # Allow unfree proprietary packages such as spotify or vscode
-  nixpkgs.config.allowUnfree = true;
-  
-  # System-wide packages
-  environment.systemPackages = with pkgs; [ 
-    # System utilities
-    ark
-    usbutils
-    unzip
-    vim
-    wget
-    # Desktop utilities
-    evince
-    firefox
-    keepassxc
-    libreoffice-fresh
-    phototonic
-    signal-desktop
-    thunderbird
-    vlc
-    # Gnome specifics and utilities
-    gnome3.gnome-tweaks
-    gnomeExtensions.dash-to-panel
-  ];
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enabling unfree packages, adding unstable channel to be able to install latest packages as user
-  environment.interactiveShellInit = ''
-    if [ ! -f ~/.config/nixpkgs/config.nix ]
-      then
-        mkdir -p ~/.config/nixpkgs/
-        echo  '{ allowUnfree = true; }' > ~/.config/nixpkgs/config.nix
-    fi
-  '';
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
 
   # List services that you want to enable:
 
-  # Limit journal size
-   services.journald = {
-    extraConfig = "SystemMaxUse=500M";
-  };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-
-  # Enable BT
-  hardware.bluetooth.enable = true;
-
-  # Enable pulseaudio with BT support
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
-  };
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable touchpad support.
-  # It is needed to explicitly disable libinput if we want to use synaptics
-  services.xserver.libinput.enable = false;
+  # services.xserver.libinput.enable = true;
 
-  # Enable Lenovo/IBM touchpad support
-  services.xserver.synaptics.enable = true;
-
-  # Enable for Gnome Desktop Environment
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "UTC";
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.##username## = {
     isNormalUser = true;
-    uid = 1000;
-    createHome = true;
-    home = "/home/##username##/";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "20.03"; # Did you read the comment?
- 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
 }
